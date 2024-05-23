ed6e3e3236e6224ce81c3f542062be12b785a8f4
DJYoko <yokochan.recurrent@gmail.com>
2024-05-23 16:26:46 +0900

    add header

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import rootComponent from './App.vue';
+import HeaderLoginInfo from '@/components/HeaderLoginInfo';
+
+createApp({
+  components: { HeaderLoginInfo },
+}).mount('#header');
 
 createApp({
   components: { rootComponent },
