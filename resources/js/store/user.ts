90cb44744739085f46818fc77c1b34bdcf41185d
DJYoko <yokochan.recurrent@gmail.com>
2024-05-28 11:19:10 +0900

    add type definition at userStore class

--- resources/js/store/user.ts
+++ resources/js/store/user.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 interface UserModel {
   name: string
@@ -17,6 +18,19 @@ export const userStore = defineStore('userStore', {
       this.name = _payload.name
       this.id = _payload.id
       this.role = _payload.role
+    },
+    async loadUser() {
+      try {
+        const { data }: AxiosResponse = await axios.get<UserModel>('/api/getData')
+        this.setUser(data)
+        return data // { message: "Real response!" }
+      } catch (err) {
+        if (err instanceof AxiosError) {
+          throw new Error(err.message)
+        } else {
+          throw new Error('An unexpected error occurred')
+        }
+      }
     },
   },
 })
